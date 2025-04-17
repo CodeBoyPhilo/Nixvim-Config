acf39c8adf54be8051d30da5e4c736142a3d3205
Philo Wu <philo.wu@codeboyphilo.ai>
2025-04-17 23:54:10 +1000

    fix lualine: ignore neotree dap

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -1,5 +1,18 @@
 {
   plugins.lualine = {
     enable = true;
+    settings = {
+      options = {
+        ignore_focus = [
+          "neo-tree"
+          "dapui_scopes"
+          "dapui_breakpoints"
+          "dapui_stacks"
+          "dapui_watches"
+          "dap-repl"
+          "dapui_console"
+        ];
+      };
+    };
   };
 }
