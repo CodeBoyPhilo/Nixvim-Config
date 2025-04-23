b7e000335047e75058801cb202b41ac3a8a9b588
Philo Wu <philo.wu@codeboyphilo.ai>
2025-04-23 22:08:08 +1000

    fix lualine: adjustments in bg color and separator

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -1,5 +1,48 @@
 {
   plugins.lualine = {
     enable = true;
+    settings = {
+      options = {
+        section_separators = {
+          left = "";
+          right = "";
+        };
+        component_separators = {
+          left = "";
+          right = "";
+        };
+      };
+      sections = {
+        lualine_c = [
+          {
+            __unkeyed-1 = "filename";
+            color = {
+              bg = "#1a1b26";
+            };
+          }
+        ];
+        lualine_x = [
+          {
+            __unkeyed-1 = "encoding";
+            color = {
+              bg = "#1a1b26";
+            };
+          }
+          {
+            __unkeyed-1 = "fileformat";
+            color = {
+              bg = "#1a1b26";
+            };
+          }
+          {
+            __unkeyed-1 = "filetype";
+            color = {
+              bg = "#1a1b26";
+            };
+            icons_enabled = true;
+          }
+        ];
+      };
+    };
   };
 }
