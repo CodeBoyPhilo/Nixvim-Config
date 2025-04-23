0b94fc78c586954c287874f422cf53af20bba2c8
Philo Wu <philo.wu@codeboyphilo.ai>
2025-04-24 03:36:03 +1000

    fix lualine: disable separator.

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -4,44 +4,13 @@
     settings = {
       options = {
         section_separators = {
-          left = "";
-          right = "";
+          left = "";
+          right = "";
         };
         component_separators = {
-          left = "";
-          right = "";
+          left = "";
+          right = "";
         };
-      };
-      sections = {
-        lualine_c = [
-          {
-            __unkeyed-1 = "filename";
-            color = {
-              bg = "#1a1b26";
-            };
-          }
-        ];
-        lualine_x = [
-          {
-            __unkeyed-1 = "encoding";
-            color = {
-              bg = "#1a1b26";
-            };
-          }
-          {
-            __unkeyed-1 = "fileformat";
-            color = {
-              bg = "#1a1b26";
-            };
-          }
-          {
-            __unkeyed-1 = "filetype";
-            color = {
-              bg = "#1a1b26";
-            };
-            icons_enabled = true;
-          }
-        ];
       };
     };
   };
