5fed10e7b49bddde1181a5e15310df347985a81e
Philo Wu <philo.wu@codeboyphilo.ai>
2025-04-25 02:05:15 +1000

    feat luasnip: enhance pyright snippet

--- config/plugins/extras/extras.nix
+++ config/plugins/extras/extras.nix
@@ -15,32 +15,33 @@
   ];
 
   extraConfigLua = ''
-        ---------- tpipeline ----------
-        -- Enable tpipeline
-        vim.g.tpipeline_active = 1
-        -- Update interval in ms
-        vim.g.tpipeline_refresh = 10
-        -- Preserve tmux status right section
-        vim.g.tpipeline_preservebg = 0
-        -- Restore cursor when exiting
-        vim.g.tpipeline_restore_on_exit = 1
-        -- Set fillchars for optimum display
-        vim.opt.fillchars:append({ eob = " " })
+            ---------- tpipeline ----------
+            -- Enable tpipeline
+            vim.g.tpipeline_active = 1
+            -- Update interval in ms
+            vim.g.tpipeline_refresh = 10
+            -- Preserve tmux status right section
+            vim.g.tpipeline_preservebg = 0
+            -- Restore cursor when exiting
+            vim.g.tpipeline_restore_on_exit = 1
+            -- Set fillchars for optimum display
+            vim.opt.fillchars:append({ eob = " " })
 
-        ---------- luasnip ----------
-        local ls = require("luasnip")
-        local s = ls.snippet
-        local t = ls.text_node
-        local i = ls.insert_node
+            ---------- luasnip ----------
+            local ls = require("luasnip")
+            local s = ls.snippet
+            local t = ls.text_node
+            local i = ls.insert_node
 
-        -- Define custom snippets
-        ls.add_snippets("toml", {
-          s("pyright", {
-            t({
-    				"[tool.pyright]",
-    				"reportAttributeAccessIssue = false",
-    				})
-          }),
-        })
+            -- Define custom snippets
+            ls.add_snippets("toml", {
+              s("pyright", {
+                t({
+        				"[tool.pyright]",
+        				"reportAttributeAccessIssue = false",
+    						"reportGeneralTypeIssues = false",
+        				})
+              }),
+            })
   '';
 }
