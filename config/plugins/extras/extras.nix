266c8d279d669ca86a91c974b0374866b05f3e3a
Philo Wu <philo.wu@codeboyphilo.ai>
2025-06-06 13:37:40 +1000

    feat: add minimap

--- config/plugins/extras/extras.nix
+++ config/plugins/extras/extras.nix
@@ -5,17 +5,10 @@
   ...
 }:
 {
-  # Add the plugins via extraPlugins
-  # extraPlugins = with pkgs.vimPlugins; [
-  #   vim-tpipeline
-  #
-  #   # mannually configure luasnip, since adding custom snippets are problematic in nixvim due to path issues.
-  #   # https://github.com/nix-community/nixvim/discussions/2162
-  #   luasnip
-  # ];
   extraPlugins = [
     pkgs.vimPlugins.vim-tpipeline
     pkgs.vimPlugins.luasnip
+    pkgs.vimPlugins.minimap-vim
     (pkgs.vimUtils.buildVimPlugin {
       name = "nvim-silicon";
       src = pkgs.fetchFromGitHub {
@@ -28,73 +21,86 @@
   ];
 
   extraConfigLua = ''
-    		---------- tpipeline ----------
-    		-- Enable tpipeline
-    		vim.g.tpipeline_active = 1
-    		-- Update interval in ms
-    		vim.g.tpipeline_refresh = 10
-    		-- Preserve tmux status right section
-    		vim.g.tpipeline_preservebg = 0
-    		-- Restore cursor when exiting
-    		vim.g.tpipeline_restore_on_exit = 1
-    		-- Set fillchars for optimum display
-    		vim.opt.fillchars:append({ eob = " " })
+    local wk = require("which-key")
 
-    		---------- luasnip ----------
-    		local ls = require("luasnip")
-    		local s = ls.snippet
-    		local t = ls.text_node
-    		local i = ls.insert_node
+    ---------- tpipeline ----------
+    -- Enable tpipeline
+    vim.g.tpipeline_active = 1
+    -- Update interval in ms
+    vim.g.tpipeline_refresh = 10
+    -- Preserve tmux status right section
+    vim.g.tpipeline_preservebg = 0
+    -- Restore cursor when exiting
+    vim.g.tpipeline_restore_on_exit = 1
+    -- Set fillchars for optimum display
+    vim.opt.fillchars:append({ eob = " " })
 
-    		-- Define custom snippets
-    		ls.add_snippets("toml", {
-    			s("pyright", {
-    				t({
-    				"[tool.pyright]",
-    				"reportAttributeAccessIssue = false",
-    				"reportGeneralTypeIssues = false",
-    				})
-    			}),
-    		})
+    ---------- minimap ----------
+    vim.g.minimap_width = 10
+    vim.g.minimap_highlight_search = 1
+    vim.g.minimap_git_colors = 1
+		vim.g.minimap_auto_start = 1
 
-    		---------- nvim-silibon ----------
-    		require("nvim-silicon").setup({
-    			font         = "Maple Mono NF CN",
-    			no_round_corner = true,
-    			theme        = "Tokyonight Night",
-    			background   = nil,
-    			pad_horiz = 0,
-    			pad_vert = 0,
-    			shadow_blur_radius = 0,
-    			shadow_offset_x = 0,
-    			shadow_offset_y = 0,
-    			shadow_color = nil,
-    			-- line_offset = function(args)
-    			-- 		return args.line1
-    			-- end,        							
-    			no_line_number = true;
-    			to_clipboard = true,
-    			window_title = function()
-    				return vim.fn.fnamemodify(
-    					vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
-    					":t"
-    				)
-    			end,
-    			output = function()
-    				return "/Users/phil_oh/Desktop/screenshot/"
-    					.. os.date("%Y-%m-%d at %H-%M-%S")
-    					.. "_code.png"
-    			end,
-    		})
+    ---------- luasnip ----------
+    local ls = require("luasnip")
+    local s = ls.snippet
+    local t = ls.text_node
+    local i = ls.insert_node
 
-    		local wk = require("which-key")
-    		wk.add({
-    				mode = { "v" },
-    				{ "<leader>s",  group = "Silicon" },
-    				{ "<leader>sc", function() require("nvim-silicon").clip() end, desc = "Copy code screenshot to clipboard" },
-    				{ "<leader>sf", function() require("nvim-silicon").file() end,  desc = "Save code screenshot as file" },
-    				{ "<leader>ss", function() require("nvim-silicon").shoot() end,  desc = "Create code screenshot" },
-    		})
+    -- Define custom snippets
+    ls.add_snippets("toml", {
+    s("pyright", {
+    	t({
+    	"[tool.pyright]",
+    	"reportAttributeAccessIssue = false",
+    	"reportGeneralTypeIssues = false",
+    	})
+    }),
+    })
 
+    ---------- nvim-silibon ----------
+    require("nvim-silicon").setup({
+    font         = "Maple Mono NF CN",
+    no_round_corner = true,
+    theme        = "Tokyonight Night",
+    background   = nil,
+    pad_horiz = 0,
+    pad_vert = 0,
+    shadow_blur_radius = 0,
+    shadow_offset_x = 0,
+    shadow_offset_y = 0,
+    shadow_color = nil,
+    no_line_number = true;
+    to_clipboard = true,
+    window_title = function()
+    	return vim.fn.fnamemodify(
+    		vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
+    		":t"
+    	)
+    end,
+    output = function()
+    	return "/Users/phil_oh/Desktop/screenshot/"
+    		.. os.date("%Y-%m-%d at %H-%M-%S")
+    		.. "_code.png"
+    end,
+    })
+
+    wk.add({
+    	mode = { "v" },
+    	{ "<leader>s",  group = "Silicon" },
+    	{ "<leader>sc", function() require("nvim-silicon").clip() end, desc = "Copy code screenshot to clipboard" },
+    	{ "<leader>sf", function() require("nvim-silicon").file() end,  desc = "Save code screenshot as file" },
+    	{ "<leader>ss", function() require("nvim-silicon").shoot() end,  desc = "Create code screenshot" },
+    })
   '';
+
+  keymaps = [
+    {
+      key = "<leader>o";
+      action = "<cmd>MinimapToggle<CR>";
+      mode = "n";
+      options.desc = "Toggle Minimap";
+    }
+  ];
+
 }
