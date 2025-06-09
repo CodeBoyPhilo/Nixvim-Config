e5e9dc4ec1903535f827483bdff32de85f7cb071
Philo Wu <philo.wu@codeboyphilo.ai>
2025-06-09 18:42:28 +1000

    fix minimap: disable auto start

--- config/plugins/extras/extras.nix
+++ config/plugins/extras/extras.nix
@@ -39,7 +39,7 @@
     vim.g.minimap_width = 10
     vim.g.minimap_highlight_search = 1
     vim.g.minimap_git_colors = 1
-		vim.g.minimap_auto_start = 1
+		vim.g.minimap_auto_start = 0
 
     ---------- luasnip ----------
     local ls = require("luasnip")
