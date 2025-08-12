a49ee54dc9ce2d8a9dd8184076ce2ef41f5a429e
Philo Wu <philo.wu@codeboyphilo.ai>
2025-08-12 14:50:48 +1000

    fix vimtex: change okular to zathura

--- config/plugins/tools/vimtex.nix
+++ config/plugins/tools/vimtex.nix
@@ -8,9 +8,7 @@ in
     enable = true;
     texlivePackage = pkgs.texlive.combined.scheme-full;
     settings = {
-		  view_method = if isDarwin then "skim" else "general";
-      view_general_viewer = "okular";
-      view_general_options = "--unique file:@pdf\#src:@line@tex";
+		  view_method = if isDarwin then "skim" else "zathura";
       compiler_latexmk = {
         aux_dir = "./aux";
         out_dir = "./out";
