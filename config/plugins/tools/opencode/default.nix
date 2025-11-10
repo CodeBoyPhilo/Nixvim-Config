de25f56eaffe118a8cfe157af985f4c8d40ff058
Philo Wu <philo.wu@codeboyphilo.ai>
2025-11-10 17:01:56 +1100

    fix: remove opencode - bad integration

--- config/plugins/tools/opencode/default.nix
+++ config/plugins/tools/opencode/default.nix
@@ -1,39 +1,42 @@
 {
-  plugins.opencode.enable = true;
+  plugins.opencode = {
+    enable = false;
+    settings = { };
+  };
 
-  keymaps = [
-    {
-      mode = [
-        "n"
-        "x"
-      ];
-      key = "<leader>oo";
-      action.__raw = ''function() require("opencode").ask("@this: ", { submit = true }) end'';
-      options = {
-        desc = "Ask opencode";
-      };
-    }
-    {
-      mode = [
-        "n"
-        "x"
-      ];
-      key = "<leader>ox";
-      action.__raw = ''function() require("opencode").select() end'';
-      options = {
-        desc = "Execute opencode action…";
-      };
-    }
-    {
-      mode = [
-        "n"
-        "x"
-      ];
-      key = "<leader>oa";
-      action.__raw = ''function() require("opencode").prompt("@this") end'';
-      options = {
-        desc = "Add to opencode";
-      };
-    }
-  ];
+  # keymaps = [
+  #   {
+  #     mode = [
+  #       "n"
+  #       "x"
+  #     ];
+  #     key = "<leader>oo";
+  #     action.__raw = ''function() require("opencode").ask("@this: ", { submit = true }) end'';
+  #     options = {
+  #       desc = "Ask opencode";
+  #     };
+  #   }
+  #   {
+  #     mode = [
+  #       "n"
+  #       "x"
+  #     ];
+  #     key = "<leader>ox";
+  #     action.__raw = ''function() require("opencode").select() end'';
+  #     options = {
+  #       desc = "Execute opencode action…";
+  #     };
+  #   }
+  #   {
+  #     mode = [
+  #       "n"
+  #       "x"
+  #     ];
+  #     key = "<leader>oa";
+  #     action.__raw = ''function() require("opencode").prompt("@this") end'';
+  #     options = {
+  #       desc = "Add to opencode";
+  #     };
+  #   }
+  # ];
 }
